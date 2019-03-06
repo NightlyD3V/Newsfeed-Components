0fa9be59f0a30c07d5c1721da7c643de92398331
NightlyD3V <TylerSpaulding95@gmail.com>
2019-03-06 23:45:43 +0000

    Final submission for 6:45

--- Article/Article.js
+++ Article/Article.js
@@ -6,7 +6,7 @@ class Article {
     this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.domElement.querySelector('.expandButton');
-    console.log(this.expandButton);
+    // console.log(this.expandButton);
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = 'expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
@@ -16,7 +16,6 @@ class Article {
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle('article');
-    TweenMax.to(menu,2,{color: white});
   }
 }
 
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -3,17 +3,24 @@ const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
   menu.classList.toggle('menu--open');
   if(menu.classList.contains('menu--open')){
+    // text.style.display = 'flex';
+    // text.style.justifyContent = 'center';
     TweenMax.from(menu,2,{left: '-250px', ease: Bounce.easeOut});
+    TweenMax.to(articles,2,{left:250, width: '50%',ease:Back.easeOut});
+  } else {
+    TweenMax.to(articles,2,{left:'0', width: '80%',ease:Back.easeOut})
   }
-  TweenMax.to(articles,2,{left:250, width: '50%',ease:Back.easeOut});
 }
-
-
+const header = document.querySelector('h1');
+window.onload = function() {
+  TweenMax.from(header,2,{x:'200px'});
+}
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector('.menu');
 
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector('.menu-button');
 
+const text = document.querySelectorAll('.text');
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', toggleMenu);
