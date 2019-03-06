9426314a4e6a8df37fe6fdb4cda987013c6f4e06
NightlyD3V <TylerSpaulding95@gmail.com>
2019-03-06 22:39:05 +0000

    Completed MVP. Forgot to make commits!

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,19 @@
 
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
+  menu.classList.toggle('menu--open');
+  if(menu.classList.contains('menu--open')){
+    TweenMax.from(menu,2,{left: '-250px', ease: Bounce.easeOut});
+  }
+  TweenMax.to(articles,2,{left:250, width: '50%',ease:Back.easeOut});
 }
 
+
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = document.querySelector('.menu');
+
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = document.querySelector('.menu-button');
+
 // Using your menuButton reference, add a click handler that calls toggleMenu
+menuButton.addEventListener('click', toggleMenu);
